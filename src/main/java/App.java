c686b1416aff2cff41566418c6afd02d4e367edb
Nataliya <nataliyamiller@Nataliyas-MacBook-Pro.local>
2015-08-14 15:51:13 -0700

    Testing the input/output results with FluentLenium

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,23 +4,35 @@ import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
-import java.util.Scanner;
+
 
 public class App {
-  public static void main (String[] args) {}
+  public static void main (String[] args) {
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+    get ("/detector", (request,response) -> {
+      Map<String, Object> model = new HashMap<String,Object>();
+      model.put("template", "templates/detector.vtl");
+
+      String userInput = request.queryParams("userinput");
+      String yourMethodName = yourMethodName(userInput);
+
+      model.put("userInput", userInput);
+      model.put("yourMethodName", yourMethodName);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
 
 
     public static String yourMethodName (String userInput){
-      // char[] vowels = {'a', 'e', 'i', 'o', 'u'};
-      // String userInput = userStringInput.toLowerCase();
       String finalString = "";
-      // Scanner scan = new Scanner(System.in);
-      // try {
-      //   String s = scan.nextLine();
-      //   int x = Integer.parseInt(s);
-      // } catch (NumberFormatException ex){}
-
-
         for (Integer i = 0; i < userInput.length(); i++) {
           if ((userInput.charAt(i) == 'a') || (userInput.charAt(i) == 'e') || (userInput.charAt(i) == 'i')|| (userInput.charAt(i) == 'o') || (userInput.charAt(i) == 'u')){
             finalString += '-';
@@ -31,8 +43,6 @@ public class App {
             finalString += userInput.charAt(i);
           }
         }
-
-
       return finalString;
     }
 
