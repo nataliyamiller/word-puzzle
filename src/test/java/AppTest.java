42f1d9c7603f2039a7afb547406191c3cb9b3aea
Nataliya <nataliyamiller@Nataliyas-MacBook-Pro.local>
2015-08-14 13:58:48 -0700

    It will update the Specs document

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -30,4 +30,11 @@ public class AppTest {
         String test = "H-ll- -d-l--";
         assertEquals(test, yourApp.yourMethodName("Hello Odilia"));
     }
+
+    @Test
+    public void ifProgramAcceptsNumbersAndSpecialCharactersInASentence_String() {
+      App yourApp = new App();
+        String test = "H-ll- -d-l-- 235*5 y--rs";
+        assertEquals(test, yourApp.yourMethodName("Hello Odilia 235*5 years"));
+    }
 }
